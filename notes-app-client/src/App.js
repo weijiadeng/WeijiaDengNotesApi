eeca6d5884385825dfebe8cf15075014942d61eb
weijiadeng <deng.weij@northeastern.edu>
2021-03-27 14:29:54 -0700

    add nav import

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Routes from "./Routes";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 function App() {
