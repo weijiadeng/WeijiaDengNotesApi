4e4733fa76d9a23cc5d9c4f4c33cfcbcfd02006f
weijiadeng <deng.weij@northeastern.edu>
2021-03-27 14:34:42 -0700

    add react-link-router

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -2,20 +2,27 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Routes from "./Routes";
 import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand href="/" className="font-weight-bold text-muted">
-          Scratch
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Scratch
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
