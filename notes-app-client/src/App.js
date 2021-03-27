ec65303cb8f6062ae556b3e9d1b9ba358c734e00
weijiadeng <deng.weij@northeastern.edu>
2021-03-27 14:28:53 -0700

    add links to navbar

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -7,10 +7,16 @@ function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
+        <Navbar.Brand href="/" className="font-weight-bold text-muted">
           Scratch
         </Navbar.Brand>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/signup">Signup</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
