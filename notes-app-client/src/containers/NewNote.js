fbf781ec2363489f756824ada49f664ef0fc6442
weijiadeng <deng.weij@northeastern.edu>
2021-03-28 14:39:23 -0700

    add s3 upload

--- notes-app-client/src/containers/NewNote.js
+++ notes-app-client/src/containers/NewNote.js
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import { API } from "aws-amplify";
 import LoaderButton from "../components/LoaderButton";
 import { onError } from "../libs/errorLib";
+import { s3Upload } from "../libs/awsLib";
 import config from "../config";
 import "./NewNote.css";
 
@@ -26,8 +27,8 @@ export default function NewNote() {
 
         if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
             alert(
-                `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-                1000000} MB.`
+                `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE / 1000000
+                } MB.`
             );
             return;
         }
@@ -35,7 +36,9 @@ export default function NewNote() {
         setIsLoading(true);
 
         try {
-            await createNote({ content });
+            const attachment = file.current ? await s3Upload(file.current) : null;
+
+            await createNote({ content, attachment });
             history.push("/");
         } catch (e) {
             onError(e);
@@ -48,7 +51,7 @@ export default function NewNote() {
             body: note
         });
     }
-    
+
     return (
         <div className="NewNote">
             <Form onSubmit={handleSubmit}>
