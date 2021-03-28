66e51af0f81fbda8d340ccabbd8b711a113d922d
weijiadeng <deng.weij@northeastern.edu>
2021-03-28 15:51:57 -0700

    add display note

--- notes-app-client/src/containers/Notes.js
+++ notes-app-client/src/containers/Notes.js
@@ -4,37 +4,124 @@ import { API, Storage } from "aws-amplify";
 import { onError } from "../libs/errorLib";
 
 export default function Notes() {
-  const file = useRef(null);
-  const { id } = useParams();
-  const history = useHistory();
-  const [note, setNote] = useState(null);
-  const [content, setContent] = useState("");
+    const file = useRef(null);
+    const { id } = useParams();
+    const history = useHistory();
+    const [note, setNote] = useState(null);
+    const [content, setContent] = useState("");
 
-  useEffect(() => {
-    function loadNote() {
-      return API.get("notes", `/notes/${id}`);
-    }
-
-    async function onLoad() {
-      try {
-        const note = await loadNote();
-        const { content, attachment } = note;
-
-        if (attachment) {
-          note.attachmentURL = await Storage.vault.get(attachment);
+    useEffect(() => {
+        function loadNote() {
+            return API.get("notes", `/notes/${id}`);
         }
 
-        setContent(content);
-        setNote(note);
-      } catch (e) {
-        onError(e);
-      }
+        async function onLoad() {
+            try {
+                const note = await loadNote();
+                const { content, attachment } = note;
+
+                if (attachment) {
+                    note.attachmentURL = await Storage.vault.get(attachment);
+                }
+
+                setContent(content);
+                setNote(note);
+            } catch (e) {
+                onError(e);
+            }
+        }
+
+        onLoad();
+    }, [id]);
+
+    function validateForm() {
+        return content.length > 0;
     }
 
-    onLoad();
-  }, [id]);
+    function formatFilename(str) {
+        return str.replace(/^\w+-/, "");
+    }
 
-  return (
-    <div className="Notes"></div>
-  );
+    function handleFileChange(event) {
+        file.current = event.target.files[0];
+    }
+
+    async function handleSubmit(event) {
+        let attachment;
+
+        event.preventDefault();
+
+        if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+            alert(
+                `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
+                1000000} MB.`
+            );
+            return;
+        }
+
+        setIsLoading(true);
+    }
+
+    async function handleDelete(event) {
+        event.preventDefault();
+
+        const confirmed = window.confirm(
+            "Are you sure you want to delete this note?"
+        );
+
+        if (!confirmed) {
+            return;
+        }
+
+        setIsDeleting(true);
+    }
+
+    return (
+        <div className="Notes">
+            {note && (
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="content">
+                        <Form.Control
+                            as="textarea"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="file">
+                        <Form.Label>Attachment</Form.Label>
+                        {note.attachment && (
+                            <p>
+                                <a
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    href={note.attachmentURL}
+                                >
+                                    {formatFilename(note.attachment)}
+                                </a>
+                            </p>
+                        )}
+                        <Form.Control onChange={handleFileChange} type="file" />
+                    </Form.Group>
+                    <LoaderButton
+                        block
+                        size="lg"
+                        type="submit"
+                        isLoading={isLoading}
+                        disabled={!validateForm()}
+                    >
+                        Save
+          </LoaderButton>
+                    <LoaderButton
+                        block
+                        size="lg"
+                        variant="danger"
+                        onClick={handleDelete}
+                        isLoading={isDeleting}
+                    >
+                        Delete
+          </LoaderButton>
+                </Form>
+            )}
+        </div>
+    );
 }
